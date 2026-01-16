e3ad8fd2ec5182ded51184f72ad10160d988aa45
Max Guryan <rhk2dx@virginia.edu>
2026-01-16 19:27:46 +0000

    The updates and changes made through daytwostuff activity

---
+++ activity.py
@@ -0,0 +1,17 @@
+# %%
+# path
+print("/workspaces/daytwostuff")
+
+# %%
+# Creating a virtual environment
+"""You do not need to put the virtual environment into the repo but you should have a requirements.txt file that has all
+of the requirements for the virtual environment"""
+
+"""
+The path is the same; /workspaces/daytwostuff
+"""
+
+
+# %% 
+import pandas as pd
+pd.read_csv()
